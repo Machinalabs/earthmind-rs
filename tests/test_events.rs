5f3539e718c467ee43d57eb92102ff932a684106
Hassel Alcalá <hassel_aurora_13@hotmail.com>
2024-06-12 12:23:18 -0600

    modidy test_events using the new name and change the standard

--- tests/test_events.rs
+++ tests/test_events.rs
@@ -4,10 +4,10 @@ use earthmind_rs::{
 };
 
 #[test]
-fn nep_format_register_miner() {
-    let expected = r#"EVENT_JSON:{"standard":"nep171","version":"1.0.0","event":"register_miner","data":[{"miner":"hassel.near"},{"miner":"edson.near"}]}"#;
+fn test_format_register_miner() {
+    let expected = r#"EVENT_JSON:{"standard":"emip001","version":"1.0.0","event":"register_miner","data":[{"miner":"hassel.near"},{"miner":"edson.near"}]}"#;
     let log = EventLog {
-        standard: "nep171".to_string(),
+        standard: "emip001".to_string(),
         version: "1.0.0".to_string(),
         event: EventLogVariant::RegisterMiner(vec![
             RegisterMinerLog {
@@ -22,10 +22,10 @@ fn nep_format_register_miner() {
 }
 
 #[test]
-fn nep_format_register_validator() {
-    let expected = r#"EVENT_JSON:{"standard":"nep171","version":"1.0.0","event":"register_validator","data":[{"validator":"hassel.near"},{"validator":"edson.near"}]}"#;
+fn test_format_register_validator() {
+    let expected = r#"EVENT_JSON:{"standard":"emip001","version":"1.0.0","event":"register_validator","data":[{"validator":"hassel.near"},{"validator":"edson.near"}]}"#;
     let log = EventLog {
-        standard: "nep171".to_string(),
+        standard: "emip001".to_string(),
         version: "1.0.0".to_string(),
         event: EventLogVariant::RegisterValidator(vec![
             RegisterValidatorLog {
@@ -40,10 +40,10 @@ fn nep_format_register_validator() {
 }
 
 #[test]
-fn nep_format_register_request() {
-    let expected = r#"EVENT_JSON:{"standard":"nep171","version":"1.0.0","event":"register_request","data":[{"request_id":"0504fbdd23f833749a13dcde971238ba62bdde0ed02ea5424f5a522f50fae726"},{"request_id":"38d15af71379737839e4738066fd4091428081d6a57498b2852337a195bc9f5f"}]}"#;
+fn test_format_register_request() {
+    let expected = r#"EVENT_JSON:{"standard":"emip001","version":"1.0.0","event":"register_request","data":[{"request_id":"0504fbdd23f833749a13dcde971238ba62bdde0ed02ea5424f5a522f50fae726"},{"request_id":"38d15af71379737839e4738066fd4091428081d6a57498b2852337a195bc9f5f"}]}"#;
     let log = EventLog {
-        standard: "nep171".to_string(),
+        standard: "emip001".to_string(),
         version: "1.0.0".to_string(),
         event: EventLogVariant::RegisterRequest(vec![
             RegisterRequestLog {
@@ -60,10 +60,10 @@ fn nep_format_register_request() {
 }
 
 #[test]
-fn nep_format_commit_miner() {
-    let expected = r#"EVENT_JSON:{"standard":"nep171","version":"1.0.0","event":"commit_miner","data":[{"request_id":"0504fbdd23f833749a13dcde971238ba62bdde0ed02ea5424f5a522f50fae726","answer":"3910deb8f11de66388bddcc1eb1bf1e33319b71a18df2c1019e6d72c6d00f464"}]}"#;
+fn test_format_commit_miner() {
+    let expected = r#"EVENT_JSON:{"standard":"emip001","version":"1.0.0","event":"commit_miner","data":[{"request_id":"0504fbdd23f833749a13dcde971238ba62bdde0ed02ea5424f5a522f50fae726","answer":"3910deb8f11de66388bddcc1eb1bf1e33319b71a18df2c1019e6d72c6d00f464"}]}"#;
     let log = EventLog {
-        standard: "nep171".to_string(),
+        standard: "emip001".to_string(),
         version: "1.0.0".to_string(),
         event: EventLogVariant::CommitMiner(vec![CommitMinerLog {
             request_id: "0504fbdd23f833749a13dcde971238ba62bdde0ed02ea5424f5a522f50fae726"
@@ -76,10 +76,10 @@ fn nep_format_commit_miner() {
 }
 
 #[test]
-fn nep_format_commit_validator() {
-    let expected = r#"EVENT_JSON:{"standard":"nep171","version":"1.0.0","event":"commit_validator","data":[{"request_id":"0504fbdd23f833749a13dcde971238ba62bdde0ed02ea5424f5a522f50fae726","answer":"3910deb8f11de66388bddcc1eb1bf1e33319b71a18df2c1019e6d72c6d00f464"}]}"#;
+fn test_format_commit_validator() {
+    let expected = r#"EVENT_JSON:{"standard":"emip001","version":"1.0.0","event":"commit_validator","data":[{"request_id":"0504fbdd23f833749a13dcde971238ba62bdde0ed02ea5424f5a522f50fae726","answer":"3910deb8f11de66388bddcc1eb1bf1e33319b71a18df2c1019e6d72c6d00f464"}]}"#;
     let log = EventLog {
-        standard: "nep171".to_string(),
+        standard: "emip001".to_string(),
         version: "1.0.0".to_string(),
         event: EventLogVariant::CommitValidator(vec![CommitValidatorLog {
             request_id: "0504fbdd23f833749a13dcde971238ba62bdde0ed02ea5424f5a522f50fae726"
@@ -92,10 +92,10 @@ fn nep_format_commit_validator() {
 }
 
 #[test]
-fn nep_format_reveal_miner() {
-    let expected = r#"EVENT_JSON:{"standard":"nep171","version":"1.0.0","event":"reveal_miner","data":[{"request_id":"0504fbdd23f833749a13dcde971238ba62bdde0ed02ea5424f5a522f50fae726","answer":true,"message":"It's a cool NFT"}]}"#;
+fn test_format_reveal_miner() {
+    let expected = r#"EVENT_JSON:{"standard":"emip001","version":"1.0.0","event":"reveal_miner","data":[{"request_id":"0504fbdd23f833749a13dcde971238ba62bdde0ed02ea5424f5a522f50fae726","answer":true,"message":"It's a cool NFT"}]}"#;
     let log = EventLog {
-        standard: "nep171".to_string(),
+        standard: "emip001".to_string(),
         version: "1.0.0".to_string(),
         event: EventLogVariant::RevealMiner(vec![RevealMinerLog {
             request_id: "0504fbdd23f833749a13dcde971238ba62bdde0ed02ea5424f5a522f50fae726"
@@ -109,10 +109,10 @@ fn nep_format_reveal_miner() {
 }
 
 #[test]
-fn nep_format_reveal_validator() {
-    let expected = r#"EVENT_JSON:{"standard":"nep171","version":"1.0.0","event":"reveal_validator","data":[{"request_id":"0504fbdd23f833749a13dcde971238ba62bdde0ed02ea5424f5a522f50fae726","answer":["hassel.near","edson.near","anne.near","bob.near","alice.near","john.near","harry.near","scott.near","felix.near","margaret.near"],"message":"It's a cool NFT"}]}"#;
+fn test_format_reveal_validator() {
+    let expected = r#"EVENT_JSON:{"standard":"emip001","version":"1.0.0","event":"reveal_validator","data":[{"request_id":"0504fbdd23f833749a13dcde971238ba62bdde0ed02ea5424f5a522f50fae726","answer":["hassel.near","edson.near","anne.near","bob.near","alice.near","john.near","harry.near","scott.near","felix.near","margaret.near"],"message":"It's a cool NFT"}]}"#;
     let log = EventLog {
-        standard: "nep171".to_string(),
+        standard: "emip001".to_string(),
         version: "1.0.0".to_string(),
         event: EventLogVariant::RevealValidator(vec![RevealValidatorLog {
             request_id: "0504fbdd23f833749a13dcde971238ba62bdde0ed02ea5424f5a522f50fae726"
